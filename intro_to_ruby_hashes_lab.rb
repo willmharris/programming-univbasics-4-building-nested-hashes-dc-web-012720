bb32ed30e00edead0389a7e53113d438c191da30
William Harris <willmharris0@gmail.com>
2020-01-21 08:34:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,5 +11,5 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-  {railroads: {pieces: 4, rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200 }, names: {reading_railroad: {"mortgage_value": "$100"}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {} } }}
+  {railroads: {pieces: 4, rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200 }, names: {reading_railroad: {"mortgage_value" => "$100"}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {} } }}
 end
